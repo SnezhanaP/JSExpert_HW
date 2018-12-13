32d7b0e958db55ac6aff3b29e3e8aa77b068ba55
SnuazhanaP <snuzhana@gmail.com>
2018-12-14 00:48:26 +0300

    Last update

---
+++ Beginner_HomeWork_2/js/main.js
@@ -0,0 +1,18 @@
+var total = 0;
+for (var i = 0; i < 15; i++) {
+	var first = Math.floor((Math.random() * 6) + 1);
+	var second = Math.floor((Math.random() * 6) + 1);
+	if (i == 8 || i == 13) {
+		continue;
+	}
+	document.getElementById("result").innerHTML += "Первая кость: " + first + "  Вторая кость: " + second + " <br>";
+	if (first == second) {
+		document.getElementById("result").innerHTML += "Выпал дубль. Число " + first + " <br><br>";
+	} else {
+		if ((first < 3 && second > 4) || (second < 3 && first > 4)) {
+			document.getElementById("result").innerHTML += "Большой разброс между костями. Разница составляет " + Math.abs(second - first) + " <br><br>"
+		}
+	}
+	total += first + second;
+}
+total > 100 ? document.getElementById("result").innerHTML += "<br>Победа, вы набрали " + total + " очков" :  document.getElementById("result").innerHTML += "<br>Вы проиграли, у вас " + total + " очков"
