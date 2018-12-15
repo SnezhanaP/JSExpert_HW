ca60c026574889c970116ad3a5d0f905cce05a8e
SnuazhanaP <snuzhana@gmail.com>
2018-12-15 12:36:57 +0300

    final update

--- Beginner_HomeWork_2/js/main.js
+++ Beginner_HomeWork_2/js/main.js
@@ -1,19 +1,19 @@
-var total = 0;
-var elemResult = document.getElementById("result");
+var total = 0, totalInner = "";
 for (var i = 0; i < 15; i++) {
 	var first = Math.floor((Math.random() * 6) + 1);
 	var second = Math.floor((Math.random() * 6) + 1);
-	if (i == 8 || i == 13) {
+	if (i === 8 || i === 13) {
 		continue;
 	}
-	elemResult.innerHTML += "Первая кость: " + first + "  Вторая кость: " + second + " <br>";
-	if (first == second) {
-		elemResult.innerHTML += "Выпал дубль. Число " + first + " <br><br>";
+	totalInner += "Первая кость: " + first + "  Вторая кость: " + second + " <br>";
+	if (first === second) {
+		totalInner += "Выпал дубль. Число " + first + " <br><br>";
 	} else {
 		if ((first < 3 && second > 4) || (second < 3 && first > 4)) {
-			elemResult.innerHTML += "Большой разброс между костями. Разница составляет " + Math.abs(second - first) + " <br><br>"
+			totalInner += "Большой разброс между костями. Разница составляет " + Math.abs(second - first) + " <br><br>"
 		}
 	}
 	total += first + second;
 }
+document.getElementById("result").innerHTML = totalInner;
 total > 100 ? elemResult.innerHTML += "<br>Победа, вы набрали " + total + " очков" :  elemResult.innerHTML += "<br>Вы проиграли, у вас " + total + " очков"
